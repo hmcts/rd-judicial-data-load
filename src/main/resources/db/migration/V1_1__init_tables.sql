3bc15d8892d00b4b5ca51ada746745dc694d0b39
AdilOoze <adil.oozeerally@hmcts.net>
2019-12-09 11:34:27 +0000

    RDCC-672 updated sql script to create complete database

--- src/main/resources/db/migration/V1_1__init_tables.sql
+++ src/main/resources/db/migration/V1_1__init_tables.sql
@@ -1,27 +1,138 @@
 -- NB Flyway requires lowercase for table names
 create schema if not exists dbjuddata;
 
-create table judicial_user(
-	sno varchar(255) not null,
-	firstName varchar(255) not null,
-	LastName varchar(255) not null,
-	Circuit varchar(255) not null,
-	Area varchar(255) null
+CREATE TABLE public.judicial_user_profile(
+	"elinks_Id" varchar(256) NOT NULL,
+	personal_code varchar(32) NOT NULL,
+	title varchar(64) NOT NULL,
+	known_as varchar(64) NOT NULL,
+	surname varchar(256) NOT NULL,
+	full_name varchar(256) NOT NULL,
+	post_nominals varchar(32),
+	contract_type varchar(32) NOT NULL,
+	work_pattern varchar(32) NOT NULL,
+	"email_Id" varchar(256) NOT NULL,
+	joining_date date,
+	last_working_date date,
+	active_flag boolean,
+	extracted_date timestamp NOT NULL,
+	created_date timestamp,
+	last_loaded_date timestamp,
+	CONSTRAINT personal_code_unique UNIQUE (personal_code)
+	 WITH (FILLFACTOR = 10),
+	CONSTRAINT "email_Id" UNIQUE ("email_Id"),
+	CONSTRAINT "elinks_Id" PRIMARY KEY ("elinks_Id")
+
 );
 
-create table judicial_office_appointment(
-	sno varchar(255) not null,
-	firstName varchar(255) not null,
-	LastName varchar(255) not null,
-	Circuit varchar(255) not null,
-	Area varchar(255) not null
+CREATE TABLE public.judicial_office_appointment(
+	"judicial_office_appointment_Id" bigint NOT NULL,
+	"elinks_Id" varchar(256) NOT NULL,
+	role_id varchar(128) NOT NULL,
+	"contract_type_Id" varchar(256),
+	"base_location_Id" varchar(256),
+	"region_Id" varchar(256),
+	is_prinicple_appointment boolean,
+	start_date date,
+	end_date date,
+	active_flag boolean,
+	extracted_date timestamp NOT NULL,
+	created_date timestamp,
+	last_loaded_date timestamp,
+	CONSTRAINT "judicial_office_appointment_Id" PRIMARY KEY ("judicial_office_appointment_Id")
+	 WITH (FILLFACTOR = 10)
+
 );
 
-create table judicial_office_authorization(
-	sno varchar(255) not null,
-	firstName varchar(255) not null,
-	LastName varchar(255) not null,
-	Circuit varchar(255) not null,
-	Area varchar(255) not null
+CREATE TABLE public.judicial_office_authorisation(
+	"judicial_office_auth_Id" bigint NOT NULL,
+	"elinks_Id" varchar(256) NOT NULL,
+	"authorisation_Id" varchar(256),
+	jurisdiction_id varchar(256) NOT NULL,
+	authorisation_date date,
+	extracted_date date NOT NULL,
+	created_date timestamp,
+	last_loaded_date timestamp,
+	CONSTRAINT jud_auth_pk PRIMARY KEY ("judicial_office_auth_Id")
+	 WITH (FILLFACTOR = 10),
+	CONSTRAINT jud_auth_jur_unique UNIQUE (jurisdiction_id)
+	 WITH (FILLFACTOR = 10)
+
 );
+CREATE TABLE public.authorisation_type(
+	"authorisation_Id" varchar(64) NOT NULL,
+	authorisation_desc_en varchar(256) NOT NULL,
+	authorisation_desc_cy varchar(256),
+	"jurisdiction_Id" varchar(64),
+	jurisdiction_desc_en varchar(256),
+	jurisdiction_desc_cy varchar(256),
+	CONSTRAINT "authorisation_Id" PRIMARY KEY ("authorisation_Id")
+
+);
+
+CREATE TABLE public.judicial_role_type(
+	role_id varchar(64) NOT NULL,
+	role_desc_en varchar(256) NOT NULL,
+	role_desc_cy varchar(256),
+	CONSTRAINT role_id PRIMARY KEY (role_id)
+
+);
+
+CREATE TABLE public.contract_type(
+	"contract_type_Id" varchar(64) NOT NULL,
+	contract_type_desc_en varchar(256) NOT NULL,
+	contract_type_desc_cy varchar(256),
+	CONSTRAINT "contract_type_Id" PRIMARY KEY ("contract_type_Id")
+
+);
+
+CREATE TABLE public.base_location_type(
+	"base_location_Id" varchar(64) NOT NULL,
+	court_name varchar(128),
+	bench varchar(128),
+	court_type varchar(128),
+	circuit varchar(128),
+	area_of_expertise varchar(128),
+	national_court_code varchar(128),
+	CONSTRAINT "base_location_Id" PRIMARY KEY ("base_location_Id")
+
+);
+
+CREATE TABLE public.region_type(
+	"region_Id" varchar(64) NOT NULL,
+	region_desc_en varchar(256) NOT NULL,
+	region_desc_cy varchar(256),
+	CONSTRAINT "region_Id" PRIMARY KEY ("region_Id")
+
+);
+
+ALTER TABLE public.judicial_office_appointment ADD CONSTRAINT "elinks_Id" FOREIGN KEY ("elinks_Id")
+REFERENCES public.judicial_user_profile ("elinks_Id") MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE public.judicial_office_appointment ADD CONSTRAINT role_id FOREIGN KEY (role_id)
+REFERENCES public.judicial_role_type (role_id) MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE public.judicial_office_appointment ADD CONSTRAINT "contract_type_Id" FOREIGN KEY ("contract_type_Id")
+REFERENCES public.contract_type ("contract_type_Id") MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE public.judicial_office_appointment ADD CONSTRAINT "base_location_Id" FOREIGN KEY ("base_location_Id")
+REFERENCES public.base_location_type ("base_location_Id") MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE public.judicial_office_appointment ADD CONSTRAINT "region_Id" FOREIGN KEY ("region_Id")
+REFERENCES public.region_type ("region_Id") MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE public.judicial_office_authorisation ADD CONSTRAINT "elinks_Id" FOREIGN KEY ("elinks_Id")
+REFERENCES public.judicial_user_profile ("elinks_Id") MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE public.judicial_office_authorisation ADD CONSTRAINT "authorisation_Id" FOREIGN KEY ("authorisation_Id")
+REFERENCES public.authorisation_type ("authorisation_Id") MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
 
