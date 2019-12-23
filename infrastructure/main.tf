fcbb37ac91df37e0939904f8a2af34f94f52e19e
abhijit <Abhijit.diwan@hmcts.net>
2019-12-23 14:18:54 +0000

    refactor: removed s2s secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,37 +13,12 @@ locals {
   non_preview_vault_name = "${var.raw_product}-${var.env}"
   key_vault_name = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
 
-  s2s_url = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
-  s2s_vault_name = "s2s-${local.local_env}"
-  s2s_vault_uri = "https://s2s-${local.local_env}.vault.azure.net/"
-
 }
 
 data "azurerm_key_vault" "rd_key_vault" {
   name = "${local.key_vault_name}"
   resource_group_name = "${local.key_vault_name}"
 }
-
-data "azurerm_key_vault" "s2s_key_vault" {
-  name = "s2s-${local.local_env}"
-  resource_group_name = "rpe-service-auth-provider-${local.local_env}"
-}
-
-data "azurerm_key_vault_secret" "s2s_microservice" {
-  name = "s2s-microservice"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
-
-data "azurerm_key_vault_secret" "s2s_url" {
-  name = "s2s-url"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
-
-
-//data "azurerm_key_vault_secret" "s2s_secret" {
-//  name = "microservicekey-rd-judicial-api"
-//  key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
-//}
 
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
@@ -123,8 +98,6 @@ module "rd_judicial_data_load" {
     POSTGRES_USERNAME = "${module.db-judicial-ref-data.user_name}"
     POSTGRES_PASSWORD = "${module.db-judicial-ref-data.postgresql_password}"
     POSTGRES_CONNECTION_OPTIONS = "?"
-
-    S2S_URL = "${data.azurerm_key_vault_secret.s2s_url.value}"
 
 
     ROOT_LOGGING_LEVEL = "${var.root_logging_level}"
