9319b3122e86489c85e50ba53f4f421fa971c61f
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2020-01-29 10:04:11 +0000

    repaced https to http

--- src/main/java/uk/gov/hmcts/reform/juddata/service/impl/PushFileToBlobServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/juddata/service/impl/PushFileToBlobServiceImpl.java
@@ -39,7 +39,7 @@ public class PushFileToBlobServiceImpl implements FilePushService {
      * @throws StorageException StorageException
      */
     public CloudBlobContainer initializeBlobContainer() throws URISyntaxException, StorageException {
-        URI blobUri = URI.create("https://rdaat.privatelink.blob.core.windows.net");
+        URI blobUri = URI.create("http://rdaat.privatelink.blob.core.windows.net");
         CloudStorageAccount cloudStorageAccount = new CloudStorageAccount(credsreg, blobUri, blobUri, blobUri);
         CloudBlobClient cloudBlobClient = cloudStorageAccount.createCloudBlobClient();
         return cloudBlobClient.getContainerReference(azureBlobConfig.getContainerName());
