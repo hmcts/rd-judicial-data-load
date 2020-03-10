4cd75e0613953865c19751b7f283de50c0caa816
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2020-03-10 11:23:40 +0000

    RDCC-1011 adding scheduler on leaf route

--- src/main/java/uk/gov/hmcts/reform/juddata/scheduler/JrdLeafDataLoadStarter.java
+++ src/main/java/uk/gov/hmcts/reform/juddata/scheduler/JrdLeafDataLoadStarter.java
@@ -1,16 +1,12 @@
 package uk.gov.hmcts.reform.juddata.scheduler;
 
-import java.util.Date;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.juddata.camel.route.LeafTableRoute;
 
@@ -30,36 +26,16 @@ public class JrdLeafDataLoadStarter {
     @Value("${start-leaf-route}")
     private String startLeafRoute;
 
-    public static final long INITIAL_DELAY_SECS = 60000L;
-
-    private TaskScheduler scheduler;
-
     @PostConstruct
     public void postConstruct() throws Exception {
         camelContext.start();
         leafTableRoutes.startRoute();
-        executeTask();
+        producerTemplate.sendBody(startLeafRoute, "starting JRD leaf routes");
     }
 
-    /**
-     * this will call camel routes once after 60 secs spring boot is started.
-     */
-    public void executeTask() {
-        ScheduledExecutorService localExecutor = Executors.newSingleThreadScheduledExecutor();
-        scheduler = new ConcurrentTaskScheduler(localExecutor);
-        scheduler.schedule(leafRoutsRunnable,
-                new Date(System.currentTimeMillis() + INITIAL_DELAY_SECS));
+    @Scheduled(cron = "${scheduler.camel-leaf-router-config}")
+    public void runJrdLeafScheduler() {
+        producerTemplate.sendBody(startLeafRoute, "starting JRD leaf routes though scheduler");
     }
 
-    Runnable leafRoutsRunnable = new Runnable() {
-        @Override
-        public void run() {
-            try {
-                log.info("calling leaf routes");
-                producerTemplate.sendBody(startLeafRoute, "starting JRD leaf routes");
-            } catch (Exception ex) {
-                log.error("some error while loading spring context", ex);
-            }
-        }
-    };
 }
