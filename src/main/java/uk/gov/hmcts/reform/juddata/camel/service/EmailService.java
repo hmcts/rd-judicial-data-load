97d997441b16159e60d7697fd1bff7c9e55db78c
abhijit <Abhijit.diwan@hmcts.net>
2020-04-16 13:30:10 +0100

    fix: email changes

---
+++ src/main/java/uk/gov/hmcts/reform/juddata/camel/service/EmailService.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.juddata.camel.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.juddata.camel.exception.EmailFailureException;
+
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class EmailService {
+
+    @Autowired
+    JavaMailSender mailSender;
+
+    public void sendEmail(final String from,EmailData emailData) {
+        try {
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(emailData.recipient);
+            message.setSubject(emailData.subject);
+            message.setText(emailData.message);
+            message.setFrom(from);
+            mailSender.send(message);
+
+        } catch (MailException e) {
+            throw new EmailFailureException(e);
+        }
+    }
+}
