cca391cb683a686efb3e1caa8441e9e4b39c2fb3
AdilOoze <adil.oozeerally@hmcts.net>
2021-01-06 16:18:11 +0000

    RDCC-2187: removing unused imports

--- src/test/java/uk/gov/hmcts/reform/juddata/camel/processor/JudicialBaseLocationProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/juddata/camel/processor/JudicialBaseLocationProcessorTest.java
@@ -21,7 +21,6 @@ import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
