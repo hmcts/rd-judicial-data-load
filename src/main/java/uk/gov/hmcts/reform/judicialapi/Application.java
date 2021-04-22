f19262cb530404dbce54a301633210dd15d1d422
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2021-04-22 11:49:36 +0100

    feat : RDCC-2721 added scheduled annotation

--- src/main/java/uk/gov/hmcts/reform/judicialapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/Application.java
@@ -5,18 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableJpaRepositories
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application  {
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    /*
-    select jup.title, jup.known_as, jup.surname, jup.email_id, jup.elinks_id, jup.object_id    from judicial_user_profile jup
-where full_name like '%Mag%' or title like '%Mag%' or email_id like '%Mag%'
-     */
 }
--- src/main/java/uk/gov/hmcts/reform/judicialapi/service/JudicialService1Impl.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/service/JudicialService1Impl.java
@@ -3,7 +3,9 @@ package uk.gov.hmcts.reform.judicialapi.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.judicialapi.domain.JudicialUserProfile;
 import uk.gov.hmcts.reform.judicialapi.repository.JudicialUserProfileRepository;
@@ -16,10 +18,19 @@ public class JudicialService1Impl {
     @Autowired
     JudicialUserProfileRepository judicialUserProfileRepository;
 
+    @Autowired
+    CacheManager cacheManager;
+
     @Cacheable(value = "autocomplete")
     public List<JudicialUserProfile> findAllJudicialUserProfiles() {
         log.info("::::: findAllJudicialUserProfiles called :::::");
         return judicialUserProfileRepository.findAllJudicialUserProfiles();
+    }
+
+    @Scheduled(cron = "0 0/3 *  * * ?")// every 3 min, we need to change to midnight 12
+    public void evictAllCacheValues() {
+        cacheManager.getCache("autocomplete").clear();
+        log.info("::::: cache evicted::::: ");
     }
 }
 
