b68905a7f3ae51aa6ba91ffdce3cddc750b19cc7
abhijit <Abhijit.diwan@hmcts.net>
2021-05-14 13:04:37 +0100

    refactor: Helm chart configuration for AKS

--- src/functionalTest/java/uk/gov/hmcts/reform/juddata/config/ParentCamelConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/juddata/config/ParentCamelConfig.java
@@ -56,7 +56,7 @@ import static org.mockito.Mockito.mock;
 @Configuration
 public class ParentCamelConfig {
 
-    @Value("${serviceBusSecret}")
+    @Value("${serviceBusSecret:''}")
     String asbConnectionStringAks;
 
     @Value("${execution_environment}")
