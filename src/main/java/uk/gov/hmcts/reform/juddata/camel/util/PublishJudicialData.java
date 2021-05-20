79f3af5f9e6c9423e43475b83d76d16da04731fc
Nitish Singla <nitish.singla@hmcts.net>
2021-05-20 11:38:48 +0100

    RDCC2688: Wrapping the Judicial User ID's

--- src/main/java/uk/gov/hmcts/reform/juddata/camel/util/PublishJudicialData.java
+++ src/main/java/uk/gov/hmcts/reform/juddata/camel/util/PublishJudicialData.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.juddata.camel.util;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +9,6 @@ import java.util.List;
 @Getter
 @Setter
 public class PublishJudicialData {
+    @JsonProperty
     private List<String> userIds;
 }
