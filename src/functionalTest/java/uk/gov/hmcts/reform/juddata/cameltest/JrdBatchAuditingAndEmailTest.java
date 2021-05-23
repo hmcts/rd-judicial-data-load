05d5a5f0ee6b9e1fb4453548529d813463d34968
abhijit <Abhijit.diwan@hmcts.net>
2021-05-23 19:48:45 +0100

    refactor: Helm chart configuration for AKS

--- src/functionalTest/java/uk/gov/hmcts/reform/juddata/cameltest/JrdBatchAuditingAndEmailTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/juddata/cameltest/JrdBatchAuditingAndEmailTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.SqlConfig;
@@ -48,6 +49,7 @@ import static uk.gov.hmcts.reform.juddata.cameltest.testsupport.ParentIntegratio
 @EnableTransactionManagement
 @SqlConfig(dataSource = "dataSource", transactionManager = "txManager",
     transactionMode = SqlConfig.TransactionMode.ISOLATED)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 class JrdBatchAuditingAndEmailTest extends JrdBatchIntegrationSupport {
 
     @Test
