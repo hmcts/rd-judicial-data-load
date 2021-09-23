f99c0ed28134057f30d92f520fb52274b1808416
shivajihmcts <shivaji.gaikwad@hmcts.net>
2021-09-23 14:31:34 +0100

    RDCC-3329: Update the 'Service_Code' field length

---
+++ src/functionalTest/resources/db/testmigration/V1_25__init_table_alter_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE judicial_role_type(
+	role_id varchar(64) NOT NULL,
+	role_desc_en varchar(256) NOT NULL,
+	role_desc_cy varchar(256),
+	CONSTRAINT role_id PRIMARY KEY (role_id)
+
+);
+
+ALTER TABLE judicial_office_appointment ALTER COLUMN service_code varchar(64);
+ALTER TABLE judicial_office_authorisation ALTER COLUMN service_code varchar(64);
+ALTER TABLE judicial_service_code_mapping ALTER COLUMN service_code varchar(64);
--- src/functionalTest/resources/db/testmigration/V1_25__init_table_judicial_role_type.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE judicial_role_type(
-	role_id varchar(64) NOT NULL,
-	role_desc_en varchar(256) NOT NULL,
-	role_desc_cy varchar(256),
-	CONSTRAINT role_id PRIMARY KEY (role_id)
-
-);
