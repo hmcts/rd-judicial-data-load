bab5ebc6c266ade212715269715216677b4ed21b
shivajihmcts <shivaji.gaikwad@hmcts.net>
2021-10-27 09:40:28 +0100

    RDCC-3526:Changes made to fix failing test cases

--- src/test/java/uk/gov/hmcts/reform/juddata/camel/processor/JudicialUserRoleTypeProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/juddata/camel/processor/JudicialUserRoleTypeProcessorTest.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.juddata.camel.processor;
 
+import com.google.common.collect.ImmutableSet;
+import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -9,6 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import uk.gov.hmcts.reform.data.ingestion.camel.route.beans.RouteProperties;
 import uk.gov.hmcts.reform.data.ingestion.camel.validator.JsrValidatorInitializer;
 import uk.gov.hmcts.reform.juddata.camel.binder.JudicialUserRoleType;
@@ -21,12 +26,18 @@ import javax.validation.ValidatorFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.spy;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
+import static uk.gov.hmcts.reform.juddata.camel.helper.JrdTestSupport.PERID_1;
+import static uk.gov.hmcts.reform.juddata.camel.helper.JrdTestSupport.PERID_2;
 import static uk.gov.hmcts.reform.data.ingestion.camel.util.MappingConstants.ROUTE_DETAILS;
 import static uk.gov.hmcts.reform.juddata.camel.helper.JrdTestSupport.createJudicialUserRoleType;
 
@@ -36,17 +47,21 @@ class JudicialUserRoleTypeProcessorTest {
 
     JsrValidatorInitializer<JudicialUserRoleType> judicialUserRoleTypeJsrValidatorInitializer;
 
-    private Validator validator;
+    JudicialUserProfileProcessor judicialUserProfileProcessor = spy(new JudicialUserProfileProcessor());
+
+    CamelContext camelContext = new DefaultCamelContext();
 
     Exchange exchangeMock;
-
     Message messageMock;
-
     Registry registryMock;
-
     ApplicationContext applicationContext = mock(ConfigurableApplicationContext.class);
-
     ConfigurableListableBeanFactory configurableListableBeanFactory = mock(ConfigurableListableBeanFactory.class);
+    final JdbcTemplate jdbcTemplate = mock(JdbcTemplate.class);
+    final PlatformTransactionManager platformTransactionManager = mock(PlatformTransactionManager.class);
+    final TransactionStatus transactionStatus = mock(TransactionStatus.class);
+
+    private Validator validator;
+
 
 
     @BeforeEach
@@ -56,22 +71,43 @@ class JudicialUserRoleTypeProcessorTest {
 
         setField(judicialUserRoleTypeProcessor,
                 "judicialUserRoleTypeJsrValidatorInitializer", judicialUserRoleTypeJsrValidatorInitializer);
+        setField(judicialUserRoleTypeProcessor, "judicialUserProfileProcessor",
+                judicialUserProfileProcessor);
 
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
         setField(judicialUserRoleTypeJsrValidatorInitializer, "validator", validator);
+        setField(judicialUserRoleTypeJsrValidatorInitializer, "camelContext", camelContext);
+        setField(judicialUserRoleTypeJsrValidatorInitializer, "jdbcTemplate", jdbcTemplate);
+        setField(judicialUserRoleTypeJsrValidatorInitializer, "platformTransactionManager",
+                platformTransactionManager);
+
         messageMock = mock(Message.class);
         RouteProperties routeProperties = new RouteProperties();
         routeProperties.setFileName("test");
         exchangeMock = mock(Exchange.class);
         registryMock = mock(Registry.class);
         when(exchangeMock.getIn()).thenReturn(messageMock);
+
         when(exchangeMock.getIn().getHeader(ROUTE_DETAILS)).thenReturn(routeProperties);
-        when(exchangeMock.getContext()).thenReturn(new DefaultCamelContext());
+        when(exchangeMock.getContext()).thenReturn(camelContext);
         when(exchangeMock.getMessage()).thenReturn(messageMock);
+
         setField(judicialUserRoleTypeProcessor, "applicationContext", applicationContext);
+        setField(judicialUserProfileProcessor, "applicationContext", applicationContext);
         when(((ConfigurableApplicationContext)
                 applicationContext).getBeanFactory()).thenReturn(configurableListableBeanFactory);
+        when(judicialUserProfileProcessor.getValidPerIdInUserProfile()).thenReturn(ImmutableSet.of(PERID_1,
+                PERID_2, "invalid"));
+
+        int[][] intArray = new int[1][];
+        when(jdbcTemplate.batchUpdate(anyString(), anyList(), anyInt(), any())).thenReturn(intArray);
+        when(platformTransactionManager.getTransaction(any())).thenReturn(transactionStatus);
+        doNothing().when(platformTransactionManager).commit(transactionStatus);
+
+
+
+
     }
 
     @Test
