98ef742fcbe70bb1bd3a046929a23ba5feea6d2f
Sourav Roy <sourav.roy@hmcts.net>
2022-01-11 13:34:33 +0000

    missing parenthesis added

--- src/test/java/uk/gov/hmcts/reform/juddata/camel/util/EmailTemplateTest.java
+++ src/test/java/uk/gov/hmcts/reform/juddata/camel/util/EmailTemplateTest.java
@@ -53,7 +53,7 @@ class EmailTemplateTest {
     void getEmailBody_should_throw_exception() throws Exception {
         when(freemarkerConfig.getTemplate(anyString())).thenThrow(IOException.class);
         assertThrows(EmailException.class,
-                () -> emailTemplate.getEmailBody("lower-level-auth.ftl", getModel());
+                () -> emailTemplate.getEmailBody("lower-level-auth.ftl", getModel()));
     }
 
     @NotNull
