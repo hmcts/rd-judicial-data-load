f157bfbdca166da8fa0d1f1866002d8a0cba31d5
shivajilokare <shivaji.lokare@HCTS.NET>
2022-03-02 13:41:40 +0000

    updated query

---
+++ src/main/resources/db/migration/V1_30__alter_judicial_user_profile.sql
@@ -0,0 +1,7 @@
+ALTER TABLE judicial_user_profile ADD COLUMN Is_Judge Boolean;
+ALTER TABLE judicial_user_profile ADD COLUMN Is_Panel_Member Boolean;
+ALTER TABLE judicial_user_profile ADD COLUMN Is_Magistrate Boolean;
+ALTER TABLE judicial_user_profile ADD COLUMN MRD_Created_Time Timestamp;
+ALTER TABLE judicial_user_profile ADD COLUMN MRD_Updated_Time Timestamp;
+ALTER TABLE judicial_user_profile ADD COLUMN MRD_Deleted_Time Timestamp;
+commit;
