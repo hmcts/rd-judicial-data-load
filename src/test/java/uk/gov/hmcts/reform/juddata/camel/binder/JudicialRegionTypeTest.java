d1f453756e6225cbce2d6085a9771acbe24d11cd
SujeethKumarAri <73644938+SujeethKumarAri@users.noreply.github.com>
2022-03-15 16:25:45 +0000

    RDCC_4201 Changes to Camel ingestion for Judicial Office Regions (#379)

--- src/test/java/uk/gov/hmcts/reform/juddata/camel/binder/JudicialRegionTypeTest.java
+++ src/test/java/uk/gov/hmcts/reform/juddata/camel/binder/JudicialRegionTypeTest.java
@@ -2,18 +2,20 @@ package uk.gov.hmcts.reform.juddata.camel.binder;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.hmcts.reform.juddata.camel.helper.JrdTestSupport.createJudicialRegionType;
 
 class JudicialRegionTypeTest {
 
     @Test
-    @SuppressWarnings("unchecked")
     void test_objects_JudicialOfficeAppointment_correctly() {
         JudicialRegionType judicialRegionType = createJudicialRegionType();
 
         assertEquals("regionId", judicialRegionType.getRegionId());
         assertEquals("region_desc_en", judicialRegionType.getRegionDescEn());
         assertEquals("region_desc_cy", judicialRegionType.getRegionDescCy());
+        assertEquals("2022-05-03 00:00:00", judicialRegionType.getMrdCreatedTime());
+        assertEquals("2022-05-01 00:00:00", judicialRegionType.getMrdUpdatedTime());
+        assertEquals("2022-05-04 00:00:00", judicialRegionType.getMrdDeletedTime());
     }
 }
