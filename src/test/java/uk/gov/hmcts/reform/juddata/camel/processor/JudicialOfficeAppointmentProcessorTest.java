0dd8be63647c1c7390b890503843aa77058c056b
shivajilokare <shivaji.lokare@HCTS.NET>
2022-04-07 13:27:50 +0100

    updated tests

--- src/test/java/uk/gov/hmcts/reform/juddata/camel/processor/JudicialOfficeAppointmentProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/juddata/camel/processor/JudicialOfficeAppointmentProcessorTest.java
@@ -195,8 +195,6 @@ class JudicialOfficeAppointmentProcessorTest {
         assertThat(((List<JudicialOfficeAppointment>) exchangeMock.getMessage().getBody()))
             .isSameAs(judicialOfficeAppointments);
         verify(judicialOfficeAppointmentProcessor).filterInvalidUserProfileRecords(any(), any(), any(), any(), any());
-        verify(judicialOfficeAppointmentJsrValidatorInitializer)
-            .auditJsrExceptions(anyList(), anyString(), anyString(), any());
         verify(judicialOfficeAppointmentProcessor).audit(any(), any());
         verify(messageMock).setBody(any());
         verify(exchangeMock, times(4)).getMessage();
