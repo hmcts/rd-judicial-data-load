27bdac362d4b22305753342d2ecbd617e1d14300
SujeethKumarAri <73644938+SujeethKumarAri@users.noreply.github.com>
2022-05-13 14:42:54 +0100

    fix: RDCC-4461 Fixed misleading file status

--- src/main/java/uk/gov/hmcts/reform/juddata/camel/processor/ICustomValidationProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/juddata/camel/processor/ICustomValidationProcessor.java
@@ -27,7 +27,6 @@ import static java.util.stream.Collectors.toSet;
 import static org.apache.camel.util.ObjectHelper.isNotEmpty;
 import static uk.gov.hmcts.reform.data.ingestion.camel.util.DataLoadUtil.getFileDetails;
 import static uk.gov.hmcts.reform.data.ingestion.camel.util.DataLoadUtil.registerFileStatusBean;
-import static uk.gov.hmcts.reform.data.ingestion.camel.util.MappingConstants.PARTIAL_SUCCESS;
 import static uk.gov.hmcts.reform.data.ingestion.camel.util.MappingConstants.ROUTE_DETAILS;
 import static uk.gov.hmcts.reform.juddata.camel.util.JrdConstants.INVALID_JSR_PARENT_ROW;
 import static uk.gov.hmcts.reform.juddata.camel.util.JrdMappingConstants.PER_ID;
