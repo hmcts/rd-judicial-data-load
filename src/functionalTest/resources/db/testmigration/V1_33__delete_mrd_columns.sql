9f4d4a8640c6b61de4d1556cfcc6eaf9e3cd8bc9
nayeemshaik-hmcts <nayeem.shaik@hmcts.net>
2022-06-09 14:43:02 +0100

    RDCC-4802/4803/4804  - scripts to revert mrd datatime changes

--- src/functionalTest/resources/db/testmigration/V1_33__delete_mrd_columns.sql
+++ src/functionalTest/resources/db/testmigration/V1_33__delete_mrd_columns.sql
@@ -44,3 +44,19 @@ ALTER TABLE region_type DROP COLUMN mrd_deleted_time;
 ALTER TABLE jrd_lrd_region_mapping DROP COLUMN mrd_created_time;
 ALTER TABLE jrd_lrd_region_mapping DROP COLUMN mrd_updated_time;
 ALTER TABLE jrd_lrd_region_mapping DROP COLUMN mrd_deleted_time;
+
+DELETE FROM judicial_service_code_mapping WHERE service_id in ('21','55');
+DELETE FROM judicial_ticket_code_mapping WHERE ticket_code in ('363','376');
+
+DELETE FROM jrd_lrd_region_mapping WHERE jrd_region_id in ('29','30','31','34');
+
+DELETE FROM judicial_location_mapping WHERE epimms_id ='736719' AND judicial_base_location_id ='916';
+DELETE FROM judicial_location_mapping WHERE epimms_id ='232580' AND judicial_base_location_id ='981';
+DELETE FROM judicial_location_mapping WHERE epimms_id ='415903' AND judicial_base_location_id ='1191';
+DELETE FROM judicial_location_mapping WHERE epimms_id ='450049' AND judicial_base_location_id ='1447';
+DELETE FROM judicial_location_mapping WHERE epimms_id ='450049' AND judicial_base_location_id ='1448';
+DELETE FROM judicial_location_mapping WHERE epimms_id ='640119' AND judicial_base_location_id ='1449';
+DELETE FROM judicial_location_mapping WHERE epimms_id ='45900' AND judicial_base_location_id ='1450';
+DELETE FROM judicial_location_mapping WHERE epimms_id ='490237' AND judicial_base_location_id ='1451';
+DELETE FROM judicial_location_mapping WHERE epimms_id ='366796' AND judicial_base_location_id ='1452';
+COMMIT;
