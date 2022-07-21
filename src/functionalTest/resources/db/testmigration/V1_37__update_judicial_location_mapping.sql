03eb5382bc94466bac197ac33abd8f1ee96e2f10
shivajilokare <shivaji.lokare@HCTS.NET>
2022-07-21 11:14:30 +0100

    updated script

---
+++ src/functionalTest/resources/db/testmigration/V1_37__update_judicial_location_mapping.sql
@@ -0,0 +1,4 @@
+UPDATE judicial_location_mapping SET epimms_id = '497583' WHERE judicial_base_location_id = '904';
+UPDATE judicial_location_mapping SET epimms_id = '214320' WHERE judicial_base_location_id = '964';
+UPDATE judicial_location_mapping SET epimms_id = '771467' WHERE judicial_base_location_id = '827';
+COMMIT;
