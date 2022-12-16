cfa7e96bc40f1f7f70a3dcc40068843cd7bae12f
hmcts <prudhvi.maddineni@hmcts.net>
2022-12-16 17:06:08 +0000

    Test Cases Added

--- src/test/java/uk/gov/hmcts/reform/juddata/camel/elinks/util/ElinkDataIngestionSchedularAuditTest.java
+++ src/test/java/uk/gov/hmcts/reform/juddata/camel/elinks/util/ElinkDataIngestionSchedularAuditTest.java
@@ -1,7 +1,8 @@
 package uk.gov.hmcts.reform.juddata.camel.elinks.util;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -19,32 +20,52 @@ import static org.powermock.api.mockito.PowerMockito.when;
 
 class ElinkDataIngestionSchedularAuditTest {
 
-    @Mock
-    private ElinkSchedularAuditRepository elinkSchedularAuditRepository;
-
-
     @InjectMocks
     ElinkDataIngestionSchedularAudit elinkDataIngestionSchedularAudit;
+    @Mock
+    private ElinkSchedularAuditRepository elinkSchedularAuditRepository;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
     }
 
-    @Test
-    @SuppressWarnings("unchecked")
-    void testSaveScheduleAudit() {
+    @ParameterizedTest
+    @CsvSource(value = {"Location:SUCCESS  ", "BaseLocation:SUCCESS", "People:SUCCESS"}, delimiter = ':')
+    void testSaveScheduleSuccessAudit(String apiName, String status) {
         ElinkDataSchedularAudit schedularAudit = new ElinkDataSchedularAudit();
         schedularAudit.setId(1);
         schedularAudit.setSchedulerName("Test User");
         schedularAudit.setSchedulerStartTime(LocalDateTime.now());
         schedularAudit.setSchedulerEndTime(LocalDateTime.now());
-        schedularAudit.setStatus("SUCCESS");
-        schedularAudit.setApiName("BaseLocations");
+        schedularAudit.setStatus(status);
+        schedularAudit.setApiName(apiName);
 
         when(elinkSchedularAuditRepository.save(any())).thenReturn(schedularAudit);
         elinkDataIngestionSchedularAudit.auditSchedulerStatus("Test User",
-            LocalDateTime.now(), LocalDateTime.now(), "SUCCESS", "BaseLocations");
+            LocalDateTime.now(), LocalDateTime.now(), schedularAudit.getStatus(), schedularAudit.getApiName());
+
+        verify(elinkSchedularAuditRepository, times(1))
+            .save(any());
+
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"Location:FAILURE  ", "BaseLocation:FAILURE", "People:FAILURE"}, delimiter = ':')
+    void testSaveScheduleFailureAudit(String apiName, String status) {
+        ElinkDataSchedularAudit schedularAudit = new ElinkDataSchedularAudit();
+        schedularAudit.setId(1);
+        schedularAudit.setSchedulerName("Test User");
+        schedularAudit.setSchedulerStartTime(LocalDateTime.now());
+        schedularAudit.setSchedulerEndTime(LocalDateTime.now().plusHours(1));
+        schedularAudit.setStatus(status);
+        schedularAudit.setApiName(apiName);
+
+        when(elinkSchedularAuditRepository.save(any())).thenReturn(schedularAudit);
+        elinkDataIngestionSchedularAudit.auditSchedulerStatus("Test User",
+            LocalDateTime.now(), LocalDateTime.now().plusHours(1),
+            schedularAudit.getStatus(), schedularAudit.getApiName());
 
         verify(elinkSchedularAuditRepository, times(1))
             .save(any());
