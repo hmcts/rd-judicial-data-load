010572fc939ed20120f6038ef74b00f9b25df9ca
hmcts <prudhvi.maddineni@hmcts.net>
2023-07-06 13:20:27 +0100

    update the row

--- src/functionalTest/resources/db/testmigration/V1_64__elinks_update_jrd_lrd_region_mapping_table.sql
+++ src/functionalTest/resources/db/testmigration/V1_64__elinks_update_jrd_lrd_region_mapping_table.sql
@@ -1,3 +1,3 @@
 -- update row script : dbjudicialdata.jrd_lrd_region_mapping
 
-UPDATE jrd_lrd_region_mapping  SET jrd_region  = 'Yorkshire and the Humber' WHERE jrd_region_id  = '32';
+UPDATE dbjudicialdata.jrd_lrd_region_mapping  SET jrd_region  = 'Yorkshire and the Humber' WHERE jrd_region_id  = '32';
