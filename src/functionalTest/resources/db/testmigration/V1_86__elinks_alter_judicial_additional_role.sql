840adb5ae31be965081bf906ca78fd089e2c749d
hmcts <prudhvi.maddineni@hmcts.net>
2023-09-21 16:34:18 +0100

    flyway changes

--- src/functionalTest/resources/db/testmigration/V1_86__elinks_alter_judicial_additional_role.sql
+++ src/functionalTest/resources/db/testmigration/V1_86__elinks_alter_judicial_additional_role.sql
@@ -1,2 +1,2 @@
 -- Adding column dbjudicialdata.judicial_additional_roles
-ALTER TABLE dbjudicialdata.judicial_additional_roles ADD COLUMN jurisdiction_role_name_id varchar(64) not null;
+ALTER TABLE dbjudicialdata.judicial_additional_roles ADD COLUMN jurisdiction_role_name_id varchar(64);
